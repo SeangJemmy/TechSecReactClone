af35c1c5154f477890f64c316b7ede4b6983dd66
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2022-02-24 14:04:04 +0700

    testing

--- src/contexts/ProductItemsContext.js
+++ src/contexts/ProductItemsContext.js
@@ -8,26 +8,27 @@ export const ListContext = createContext({
 
 export const ListContextProvider = ({ children }) => {
   const [productData, setPD] = useState(
-    null
-    //   [
-    //   {
-    //     product_id: "001",
-    //     product_name: "Reload Your Browser",
-    //     product_category: "phone",
-    //     product_price: "0",
-    //     product_discount_price: "0",
-    //     image: "./logo512.png",
-    //     product_brand: "apple",
-    //     product_verified: "true",
-    //   },
-    // ]
+    //
+    [
+      {
+        product_id: "001",
+        product_name: "Reload Your Browser",
+        product_category: "phone",
+        product_price: "0",
+        product_discount_price: "0",
+        image: "./logo512.png",
+        product_brand: "apple",
+        product_verified: "true",
+      },
+    ]
+    //
   );
 
   useEffect(() => {
     api
       .get(``)
       .then((res) => {
-        setPD(res.data.productitems);
+        setPD(() => res.data.productitems);
       })
       .catch((er) => console.log(er));
   }, []);
