0d7624f0e61c7344714bb1e485064bbdb413dc23
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2022-02-24 07:02:30 +0700

    trying different api hosts

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://techsec.free.beeceptor.com/api",
-  method: "get",
+  // baseURL: "https://techsec.free.beeceptor.com/api",
+  baseURL: "https://run.mocky.io/v3/438f17f3-3e1f-4fc4-ab6d-cf55838b4e33",
   timeout: 1000,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });
 
