234fd27b517cdffe2c276d10c0b3e89567ed23c0
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2022-02-24 10:04:01 +0700

    testing consumer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import "../App.css";
 import SetRouteTitle from "../components/SetRouteTitle";
 import AppleProduct from "../components/AppleProduct";
@@ -6,28 +6,27 @@ import { ListContext } from "../contexts/ProductItemsContext";
 
 function Home() {
   SetRouteTitle("TechSec ~ We Got What You Need");
-  const { productData, setPD } = useContext(ListContext);
-
-  useEffect(() => {
-    setPD(productData);
-  }, [productData]);
 
   return (
-    <div>
-      <div className="container m-0 p-0 mx-auto">
-        <img
-          className="image-fluid"
-          alt=""
-          width="100%"
-          height="100%"
-          src="https://techsec.cf/images/banner.jpg"
-        />
-      </div>
-      <h2 className="font-weight-bold text-center mt-4 mb-3">
-        Apple iPhones On Sale
-      </h2>
-      <AppleProduct data={productData} />
-    </div>
+    <ListContext.Consumer>
+      {(props) => (
+        <div>
+          <div className="container m-0 p-0 mx-auto">
+            <img
+              className="image-fluid"
+              alt=""
+              width="100%"
+              height="100%"
+              src="https://techsec.cf/images/banner.jpg"
+            />
+          </div>
+          <h2 className="font-weight-bold text-center mt-4 mb-3">
+            Apple iPhones On Sale
+          </h2>
+          <AppleProduct data={props.productData} />
+        </div>
+      )}
+    </ListContext.Consumer>
   );
 }
 
