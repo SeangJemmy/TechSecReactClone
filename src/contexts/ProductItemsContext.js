37177cf9ee130ae7adad296010743adb7fc2fe89
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2022-02-24 11:17:42 +0700

    testing

--- src/contexts/ProductItemsContext.js
+++ src/contexts/ProductItemsContext.js
@@ -2,23 +2,26 @@ import React, { createContext, useState, useEffect } from "react";
 import api from "../api/axios";
 
 export const ListContext = createContext({
-  productData: [],
+  productData: null,
   setPD: (data) => {},
 });
 
 export const ListContextProvider = ({ children }) => {
-  const [productData, setPD] = useState([
-    {
-      product_id: "001",
-      product_name: "Reload Your Browser",
-      product_category: "phone",
-      product_price: "0",
-      product_discount_price: "0",
-      image: "./logo512.png",
-      product_brand: "apple",
-      product_verified: "true",
-    },
-  ]);
+  const [productData, setPD] = useState(
+    null
+    //   [
+    //   {
+    //     product_id: "001",
+    //     product_name: "Reload Your Browser",
+    //     product_category: "phone",
+    //     product_price: "0",
+    //     product_discount_price: "0",
+    //     image: "./logo512.png",
+    //     product_brand: "apple",
+    //     product_verified: "true",
+    //   },
+    // ]
+  );
 
   useEffect(() => {
     api
