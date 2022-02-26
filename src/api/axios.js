b049b6de367671fc04ad0d3e9b24689d9c9f4856
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2022-02-26 09:31:27 +0700

    change request api server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://techsec.free.beeceptor.com",
+  baseURL: "https://techsec.free.beeceptor.com",
   // baseURL: "https://techsecapi.free.beeceptor.com",
   // baseURL: "https://techsecapi2.free.beeceptor.com",
-  baseURL: "https://techsecapi3.free.beeceptor.com",
+  // baseURL: "https://techsecapi3.free.beeceptor.com",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
