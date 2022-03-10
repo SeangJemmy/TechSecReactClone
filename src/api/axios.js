cd5e06d941acf5c916882b35ef13a23250884c55
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2022-03-10 14:02:29 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://techsec.free.beeceptor.com",
-  baseURL: "https://techsecapi.free.beeceptor.com",
+  // baseURL: "https://techsecapi.free.beeceptor.com",
   // baseURL: "https://techsecapi2.free.beeceptor.com",
-  // baseURL: "https://techsecapi3.free.beeceptor.com",
+  baseURL: "https://techsecapi3.free.beeceptor.com",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
