1d553735cc0dac1567b11dfff6c597e2d0e7a90a
Jemmy Seang <SeangJemmy@users.noreply.github.com>
2024-07-11 18:41:08 +0700

    Update ProductItemsContext.js

--- src/contexts/ProductItemsContext.js
+++ src/contexts/ProductItemsContext.js
@@ -14,19 +14,33 @@ export const ListContextProvider = ({ children }) => {
   const [productData, setPD] = useState([]);
 
   useEffect(() => {
-    api
-      .get()
-      .then((res) => {
-        setPD(() => res.data.productitems);
-        console.log("API Responded: " + Date());
-      })
-      .catch((er) => {
-        alert(
-          `There was a problem retreiving data from api server. Try reloading your browser! ~TechSec\nError Code: ${er}`
+        fetch('/db.json')
+            .then(response => response.json())
+            .then(data => {
+                setPD(data.productitems);
+            })
+            .catch(error => {
+               alert(
+          `There was a problem retrieving data from api server. Try reloading your browser! ~TechSec\nError Code: ${er}`
         );
-        console.log(er);
-      });
-  }, []);
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+  
+  // useEffect(() => {
+  //   api
+  //     .get()
+  //     .then((res) => {
+  //       setPD(() => res.data.productitems);
+  //       console.log("API Responded: " + Date());
+  //     })
+  //     .catch((er) => {
+        // alert(
+        //   `There was a problem retreiving data from api server. Try reloading your browser! ~TechSec\nError Code: ${er}`
+        // );
+  //       console.log(er);
+  //     });
+  // }, []);
 
   return (
     <ListContext.Provider value={{ productData, setPD }}>
